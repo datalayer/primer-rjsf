1a79f4552ebecaf49a937a5be2810a0ca6e70535
Eric Charles <eric@datalayer.io>
2024-10-01 17:09:26 +0200

    chore example with uiSchema

--- src/examples/FormExample1.tsx
+++ src/examples/FormExample1.tsx
@@ -1,29 +1,44 @@
-import Form from "./../index";
 import { RJSFSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
+import { ThemeProvider, Box } from "@primer/react";
+import Form from "./../index";
 
 const schema: RJSFSchema = {
-  title: "Todo",
+  title: "Todo Example",
   type: "object",
-  required: ["title"],
+  required: [
+    "title"
+  ],
   properties: {
     title: {type: "string", title: "Title", default: "A new task"},
     hello: {type: "string", title: "Hello", default: "How are you?"},
+    details: {type: "string", title: "Details", default: "Roundhouse kicking asses since 1940"},
     done: {type: "boolean", title: "Done?", default: false}
+  }
+};
+
+const uiSchema: RJSFSchema = {
+  bio: {
+    "ui:widget": "textarea"
   }
 };
 
 const FormExample1 = () => {
   return (
-    <Form
-      schema={schema}
-      validator={validator}
-      onSubmit={({ formData }, e) => {
-        console.log("submitted formData", formData);
-        console.log("submit event", e);
-        window.alert("Form submitted");
-      }}
-    />
+    <ThemeProvider>
+      <Box m={5}>
+        <Form
+          schema={schema}
+          uiSchema={uiSchema}
+          validator={validator}
+          onSubmit={({ formData }, e) => {
+            console.log("submitted formData", formData);
+            console.log("submit event", e);
+            window.alert("Form submitted");
+          }}
+        />
+      </Box>
+    </ThemeProvider>
   )
 }
 
